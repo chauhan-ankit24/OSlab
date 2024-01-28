78d706bd90323d4a3e7b95f8d90c6ceccb9c391b
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-01-28 11:07:18 +0530

    chapte15

--- sampleLab.c
+++ sampleLab.c
@@ -10,6 +10,7 @@
 // ctrl + x - back
 // ctrl + l - clear
 // ps - which process are runnning in system
+//
 
 // File descriptor  // 0 stdin - keyboard
 // 1 stdout - on screen
@@ -567,3 +568,22 @@ void *fun2()
 // Value of shared variable updated by Thread2 is: 1
 // Thread2 released the lock
 // Final value of shared is 1
+
+// ************************* InterProcess Communication : popen/pclose *************************
+// popen used to send / recieve from a process
+// popen opens a pipe stream i.e, unidirectional
+// 1st process will be the mentioned and second will be the mentioned
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+int main()
+{
+    FILE *rd;
+    char buffer[50];
+    sprintf(buffer, "name first");
+    rd = popen("wc -c", "w");                         // wc -c -> is the process which counts the number of characters passed. 2nd parameter is "w" which means pipe is opened in writing mode
+    fwrite(buffer, sizeof(char), strlen(buffer), rd); // to write the data into the pipe
+    pclose(rd);
+}
